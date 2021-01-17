547df93e7719d1722f98fb6857abbd78f380dc48
ando116 <dai-wbc.29613@outlook.jp>
2021-01-17 12:49:00 +0900

    コミット

---
+++ 1_workspace/1/カウンター/main.js
@@ -0,0 +1,10 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+
+let count = 0
+
+//ボタンをクリックしてときの処理を登録
+plusButton.onclick=function(){
+  count += 1
+  display.textContent = count
+}
