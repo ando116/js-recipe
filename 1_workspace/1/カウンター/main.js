e7f21326e32016dfdad9624af447a9758cbd7c52
ando116 <dai-wbc.29613@outlook.jp>
2021-01-17 13:28:11 +0900

    コミット

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,10 +1,22 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const twotimesButton = document.getElementById("two-times-button")
 
 let count = 0
 
 //ボタンをクリックしてときの処理を登録
 plusButton.onclick=function(){
   count += 1
+  display.textContent = count
+}
+
+minusButton.onclick=function(){
+  count -= 1
+  display.textContent = count
+}
+
+twotimesButton.onclick=function(){
+  count *= 2
   display.textContent = count
 }
